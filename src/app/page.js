49bee6f367fc9de9d4de2b9f1c7ef99017746ac7
adnin <adnin@base.co.id>
2025-02-13 22:09:31 +0700

    add cors to golang

--- src/app/page.js
+++ src/app/page.js
@@ -10,8 +10,7 @@ export default function Home() {
     fetch('http://localhost:8080/meetings')
       .then(res => res.json())
       .then(data => {
-          console.log("masuk")
-          setMeetings(data)
+          setMeetings(data.meetings)
       }
     );
   }, []);
